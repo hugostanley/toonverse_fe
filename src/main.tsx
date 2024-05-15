9e3c63703621aae7676071b3474b66a06df34106
cyrilcero <cyril.cero@gmail.com>
2024-05-15 14:49:43 +0800

    feat: add placeholder pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import LandingPage from "@pages/LandingPage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App></App>,
+    element: <LandingPage></LandingPage>,
   },
 ]);
 
