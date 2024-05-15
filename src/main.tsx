7415682981579427477aee328f5a3bf12bc3a99e
cyrilcero <cyril.cero@gmail.com>
2024-05-15 18:40:43 +0800

    fix: change tsconfig to simplify the aliases

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import LandingPage from "@pages/LandingPage";
+import { LandingPage } from "@pages";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
