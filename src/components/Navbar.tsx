0532f363af4e554027ff6cb1bc628ba1c027b7e3
jewlmils <st.jewelmilagrosa@gmail.com>
2024-05-24 14:24:21 +0800

    fix: resolve issue with Navbar links not scrolling to sections

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,27 @@
-// import React from 'react'
+import { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
+function useScrollToHash() {
+  const { hash } = useLocation();
+
+  useEffect(() => {
+    if (hash) {
+      const element = document.querySelector(hash);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [hash]);
+}
 
 function Navbar() {
+  useScrollToHash();
   return (
     <div className="sticky w-full top-0 z-50">
-      <nav className="bg-dark  shadow-lg w-100 px-8 md:px-auto">
+      <nav className="bg-dark shadow-lg w-100 px-8 md:px-auto">
         <div className="md:h-16 h-28 mx-auto container flex items-center justify-between flex-wrap">
-          {/* logo */}
           <div className="text-indigo-500">
             <img
               src="/src/assets/temp-logo-white.png"
@@ -16,27 +29,22 @@ function Navbar() {
               className="h-8 rounded-full"
             />
           </div>
-          {/* nav links */}
           <div className="text-gray-200 w-full md:w-auto">
             <ul className="flex font-semibold justify-between">
-              {/* Active Link: text-indigo-500, Inactive Link: hover:text-indigo-500 */}
               <li className="md:px-4 md:py-2 hover:text-pink">
-                <a href="#">
-                  <Link to="/">Home</Link>
-                </a>
+                <Link to="/">Home</Link>
               </li>
               <li className="md:px-4 md:py-2 hover:text-pink">
-                <a href="#styles">Catalog</a>
+                <Link to="/#styles">Catalog</Link>
               </li>
               <li className="md:px-4 md:py-2 hover:text-pink">
-                <a href="#faqs">FAQS</a>
+                <Link to="/#faqs">FAQS</Link>
               </li>
               <li className="md:px-4 md:py-2 hover:text-pink">
-                <a href="#">Contact Us</a>
+                <Link to="/#contact">Contact Us</Link>
               </li>
             </ul>
           </div>
-          {/* icons */}
           <div className="text-light flex gap-3">
             <FontAwesomeIcon icon={faUser} className="h-6" />
             <FontAwesomeIcon icon={faCartShopping} className="h-6" />
