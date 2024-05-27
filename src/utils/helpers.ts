dc1fcfae95bd559ef6b1ad52cb2bf5b3f2a0b186
cyrilcero <cyril.cero@gmail.com>
2024-05-27 14:48:45 +0800

    feat: add helper for time formatting

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -7,3 +7,5 @@ export function getLocalStorage(key: string): any | null {
 export function setLocalStorage(key: string, value: any): void {
   localStorage.setItem(key, JSON.stringify(value))
 }
+
+export const formatCreatedAt = (date: string) => new Date(date).toUTCString();
