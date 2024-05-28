dc56c748ac07ca682ff61375edba605c7cba2125
cyrilcero <cyril.cero@gmail.com>
2024-05-29 01:40:01 +0800

    feat: add orders table in account

--- src/pages/users/account/UserAccount.tsx
+++ src/pages/users/account/UserAccount.tsx
@@ -1,50 +1,93 @@
 import { Link } from "react-router-dom";
 import { useUserProfile, useUserData } from "@layouts";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPenToSquare, faArrowRightToBracket } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPenToSquare,
+  faArrowRightToBracket,
+} from "@fortawesome/free-solid-svg-icons";
 import { Spinner, LogoutBtn } from "@components";
+import { useQuery } from "@tanstack/react-query";
+import { ALL_ORDERS, apiClient } from "@utils";
 import { LOGOUT_URL } from "@utils";
+import { UserOrdersTable } from "@pages";
+
+type Order = {
+  id: number;
+  item_id: number;
+  payment_id: number;
+  workforce_id: number;
+  amount: string;
+  order_status: string;
+  background_url: string;
+  number_of_heads: string;
+  picture_style: string;
+  art_style: string;
+  notes?: string | null;
+  reference_image: string;
+  latest_artwork?: string | null;
+  latest_artwork_revision?: string | null;
+  created_at: string;
+  updated_at: string;
+};
 
 function UserAccount() {
   const { userData } = useUserData();
   const { data: userProfile, isPending } = useUserProfile();
+  const { data, isLoading } = useQuery<Order[]>({
+    queryKey: ["allOrders"],
+    queryFn: async () => {
+      const response = await apiClient.get(ALL_ORDERS);
+      return response.data;
+    },
+  });
 
-  // console.log('USER:', userProfile)
   if (isPending || !userData) {
     return (
-      <section className='w-full h-full p-2 px-4 flex flex-col gap-2'>
+      <section className="w-full h-full p-2 px-4 flex flex-col gap-2">
         <Spinner />
       </section>
     );
   }
 
   return (
-    <section className='w-full h-full p-2 px-4 flex flex-col gap-2'>
-      <div className='w-1/4 flex flex-col gap-3'>
-        <div className='py-2 border-b-2 border-gray-400/60 flex text-3xl font-bold font-header'>
+    <section className="w-full h-full p-2 px-4 flex gap-2">
+      <div className="w-1/4 flex flex-col gap-3">
+        <div className="py-2 border-b-2 border-gray-400/60 flex text-3xl font-bold font-header">
           <h1>Account Information</h1>
-          <Link to='edit' className="flex items-center justify-center px-3 text-dark/45 hover:text-dark">
-            <FontAwesomeIcon icon={faPenToSquare} className='h-1/2' />
+          <Link
+            to="edit"
+            className="flex items-center justify-center px-3 text-dark/45 hover:text-dark"
+          >
+            <FontAwesomeIcon icon={faPenToSquare} className="h-1/2" />
           </Link>
         </div>
 
         <div className="flex flex-col gap-1">
-          <h2 className='text-xl font-bold'>{`${userProfile?.first_name} ${userProfile?.last_name}`}</h2>
-          <span className='text-sm'>
+          <h2 className="text-xl font-bold">{`${userProfile?.first_name} ${userProfile?.last_name}`}</h2>
+          <span className="text-sm">
             BIlling Address: {userProfile?.billing_address}
           </span>
         </div>
 
         <small className="flex gap-2 items-center">
           <FontAwesomeIcon icon={faArrowRightToBracket} />
-          <LogoutBtn apiUrl={LOGOUT_URL} redirectPath='/login' className='font-light hover:underline underline-offset-4' />
+          <LogoutBtn
+            apiUrl={LOGOUT_URL}
+            redirectPath="/login"
+            className="btn__primary bg-pink"
+          />
         </small>
       </div>
-      
-      
-      
+      <div className="w-3/4">
+        <h1 className="py-2 border-b-2 border-gray-400/60 flex text-3xl font-bold font-header">
+          Orders
+        </h1>
+        <div className="py-2">
+          <UserOrdersTable data={data ?? []} isLoading={isLoading} />
+        </div>
+      </div>
     </section>
   );
 }
 
-export default UserAccount
+export default UserAccount;
