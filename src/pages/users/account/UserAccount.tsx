00f25d34d683e8d683da8e7e6b5cd1747ab588f7
Louise Dungca <dungca.louisechristine@gmail.com>
2024-05-28 20:21:33 +0800

    fix: reposition logout button

--- src/pages/users/account/UserAccount.tsx
+++ src/pages/users/account/UserAccount.tsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
 import { useUserProfile, useUserData } from "@layouts";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
-import { Spinner } from "@components";
+import { faPenToSquare, faArrowRightToBracket } from "@fortawesome/free-solid-svg-icons";
+import { Spinner, LogoutBtn } from "@components";
+import { LOGOUT_URL } from "@utils";
 
 function UserAccount() {
   const { userData } = useUserData();
@@ -11,31 +12,39 @@ function UserAccount() {
   // console.log('USER:', userProfile)
   if (isPending || !userData) {
     return (
-      <section className="w-full h-full p-2 px-4 flex flex-col gap-2">
+      <section className='w-full h-full p-2 px-4 flex flex-col gap-2'>
         <Spinner />
       </section>
     );
   }
 
   return (
-    <section className="w-full h-full p-2 px-4 flex flex-col gap-2">
-      <div className="w-1/4 flex flex-col gap-3">
-        <h1 className="py-2 border-b-2 border-gray-400/60 flex justify-between text-3xl font-bold font-header">
-          Account Information
-          <Link to="edit" className="p-2">
-            <small className="font-semibold text-sm">
-              <FontAwesomeIcon icon={faPenToSquare} className="h-1/2" />
-            </small>
+    <section className='w-full h-full p-2 px-4 flex flex-col gap-2'>
+      <div className='w-1/4 flex flex-col gap-3'>
+        <div className='py-2 border-b-2 border-gray-400/60 flex text-3xl font-bold font-header'>
+          <h1>Account Information</h1>
+          <Link to='edit' className="flex items-center justify-center px-3 text-dark/45 hover:text-dark">
+            <FontAwesomeIcon icon={faPenToSquare} className='h-1/2' />
           </Link>
-        </h1>
+        </div>
 
-        <h2 className="text-xl font-bold">{`${userProfile?.first_name} ${userProfile?.last_name}`}</h2>
-        <small className="">
-          BIlling Address: {userProfile?.billing_address}
+        <div className="flex flex-col gap-1">
+          <h2 className='text-xl font-bold'>{`${userProfile?.first_name} ${userProfile?.last_name}`}</h2>
+          <span className='text-sm'>
+            BIlling Address: {userProfile?.billing_address}
+          </span>
+        </div>
+
+        <small className="flex gap-2 items-center">
+          <FontAwesomeIcon icon={faArrowRightToBracket} />
+          <LogoutBtn apiUrl={LOGOUT_URL} redirectPath='/login' className='font-light hover:underline underline-offset-4' />
         </small>
       </div>
+      
+      
+      
     </section>
   );
 }
 
-export default UserAccount;
+export default UserAccount
