3e5386bd057b8460c8d047ec202efc519261f5db
jeanbejar <jeanevabejar@gmail.com>
2024-05-28 20:18:56 +0800

    style: modify artist dashboard.

--- src/pages/workforce/artist/ArtistDashboard.tsx
+++ src/pages/workforce/artist/ArtistDashboard.tsx
@@ -4,6 +4,14 @@ import { apiClient } from "@utils";
 import { ALL_ARTISTS } from "@utils";
 import ArtistProfileInfo from "./ArtistProfileInfo"; // Import the component
 import NewArtistForm from "./NewArtistForm";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPesoSign,
+  faMoneyBillTrendUp,
+  faBagShopping,
+  faPenNib,
+} from "@fortawesome/free-solid-svg-icons";
+import OrdersTable from "../admin/OrdersTable";
 
 type Artist = {
   email: string;
@@ -23,6 +31,26 @@ function ArtistDashboard() {
   const [visible, setVisible] = useState(false);
   const [artistData, setArtistData] = useState<Artist | null>(null);
 
+  const [currentDateTime, setCurrentDateTime] = useState("");
+
+  useEffect(() => {
+    const updateDateTime = () => {
+      const options: any = {
+        weekday: "short",
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+      };
+      const now = new Date().toLocaleString("en-US", options);
+      setCurrentDateTime(now);
+    };
+
+    updateDateTime();
+    const intervalId = setInterval(updateDateTime, 60000); // Update every minute
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   const { data, isLoading, error } = useQuery({
     queryKey: ["ArtistProfile"],
     queryFn: async () => {
@@ -37,73 +65,112 @@ function ArtistDashboard() {
     }
   }, [data, artistData]);
 
-  const formattedEarnings =
-    artistData && typeof artistData.total_earnings === "number"
-      ? artistData.total_earnings.toFixed(2)
-      : "0.00";
-
   return (
-    <main >
+    <main>
       <div className="full-size flex-center flex-row bg-ivory relative">
         {isLoading ? (
           <div className="loader"></div>
         ) : error ? (
           <div className="error">An error occurred while fetching data.</div>
-        ) : artistData ? ( 
+        ) : artistData ? (
           <>
-          {/* sidebar */}
+            {/* sidebar */}
             <div
               className={`absolute top-0 ${
                 visible ? "right-0" : "hidden"
               } w-[40%] max-w-[40%] h-screen `}
             >
-              <div className="absolute right-0 w-[90%] h-screen bg-green rounded-tl-2xl rounded-bl-2xl flex-center z-10">
+              <div className="absolute right-0 w-[90%] h-screen bg-green rounded-tl-2xl rounded-bl-2xl flex-center z-20">
                 <ArtistProfileInfo artistData={artistData} />
               </div>
               <button
                 className="absolute left-0 top-16 w-[10%] h-[20vh] bg-green rounded-tl-xl rounded-bl-xl flex-center"
                 onClick={() => setVisible(false)}
               >
-                
-                  <img src="/src/assets/profile-icon.png" alt="profile-icon" className="w-[60%] h-[5vh] rounded-full border-2 border-white" />
-                
+                <img
+                  src="/src/assets/profile-icon.png"
+                  alt="profile-icon"
+                  className="w-[60%] h-[5vh] rounded-full border-2 border-white"
+                />
               </button>
             </div>
             <button
               className="absolute right-0 top-16 w-[5%] h-[20vh] bg-green rounded-tl-xl rounded-bl-xl flex-center shadow-md shadow-black"
               onClick={() => setVisible(true)}
             >
-           
-                <img src="/src/assets/profile-icon.png" alt="profile-icon" className="w-[50%] h-[5vh] rounded-full border-2 border-white"/>
-         
+              <img
+                src="/src/assets/profile-icon.png"
+                alt="profile-icon"
+                className="w-[50%] h-[5vh] rounded-full border-2 border-white"
+              />
             </button>
 
-              {/* dashboard */}
-            <div className="w-[65%] h-screen flex-center flex-col">
-              <div className="w-[90%] h-[20vh] flex-center">
-                <h1 className="text-[3rem] capitalize font-extrabold">
-                  Hello, {artistData?.first_name}!
+            {/* dashboard */}
+            <div className=" full-size">
+              <div className="absolute left-0 w-[20%] h-screen bg-yellow flex-center items-start flex-col gap-16 ">
+                <h1 className=" w-[60%] h-[25vh] p-8 capitalize text-[2rem] text-justify flex items-center font-semibold">
+                  Hello, {artistData.first_name}!
                 </h1>
-              </div>
-              <div className="w-[90%] h-[40vh] rounded-2xl border-4 border-green flex-center relative">
-                <h1 className="absolute top-4 right-8 text-[2.5rem] font-bold">Total Earnings</h1>
-                <h1 className="absolute right-32 text-[3.5rem] font-extrabold">₱ {formattedEarnings}</h1>
-                <h1 className="absolute bottom-16 right-40 text-[1.2rem]">+{formattedEarnings} {artistData.updated_at}</h1>
-              </div>
-              <div className="w-[90%] h-[40vh] flex-center flex-row justify-evenly">
-                <div className="w-[30%] h-[20vh] border-4 border-green rounded-2xl shadow-md shadow-dark flex-center">
-                  Pending Order
-                </div>
-                <div className="w-[30%] h-[20vh] border-4 border-green rounded-2xl shadow-md shadow-dark flex-center">
-                  Job Done
+                <div className="w-full h-[50vh] text-left px-8 flex flex-col gap-2">
+                  <h1 className="text-[2rem] font-bold  ">{currentDateTime}</h1>
+                  <h1 className="text-[1.3rem]">To-do Revision</h1>
+                  <h1 className="font-bold">newest</h1>
+                  <hr  className=" border-1 border-black border-dashed"/>
+                  <h1 className="w-full h-[2vh] truncate">
+                    Job# : Order Remark this is a link.....
+                  </h1>
                 </div>
               </div>
-            </div>
-            <div className="w-[35%] h-screen flex-center">
-              <div className="w-[95%] h-[90vh] border-4 border-green rounded-2xl flex-center flex-col">
-                <img src="/src/assets/temp-logo.png" alt="logo" className="w-[70%] "/>
-                <div className="w-full h-[80vh]"></div>
+              <img
+                src="/src/assets/art-pad.png"
+                alt=""
+                className="absolute right-6 bottom-0 opacity-60 "
+              />
+              <div className="w-full h-[40vh] flex-center flex-row gap-24 pl-14">
+                <div className="grid-green w-[22%] h-[20vh] z-10 bg-ivory">
+                  <FontAwesomeIcon
+                    icon={faMoneyBillTrendUp}
+                    className="icon--rounded"
+                  />
+                  <div className="flex flex-col gap-2">
+                    <h2 className="text-xl tracking-wider">Total Profit</h2>
+                    <h1 className="text-3xl tracking-wider font-bold flex gap-2">
+                      <FontAwesomeIcon icon={faPesoSign} />
+                      0.00
+                    </h1>
+                  </div>
+                </div>
+
+                <div className="grid-green w-[22%] h-[20vh]">
+                  <FontAwesomeIcon
+                    icon={faBagShopping}
+                    className="icon--rounded"
+                  />
+                  <div className="flex flex-col gap-2">
+                    <h2 className="text-xl tracking-wider">Pending Orders</h2>
+                    <h1 className="text-3xl tracking-wider font-bold">0</h1>
+                  </div>
+                </div>
+
+                <div className="grid-green w-[22%] h-[20vh]">
+                  <FontAwesomeIcon icon={faPenNib} className="icon--rounded" />
+                  <div className="flex flex-col gap-2">
+                    <h2 className="text-xl tracking-wider">In Progress</h2>
+                    <h1 className="text-3xl tracking-wider font-bold">0</h1>
+                  </div>
+                </div>
               </div>
+              <div className="w-full h-[50vh] flex justify-end  pr-40 ">
+                 <div className="w-[70%] col-span-4 row-span-4 col-start-2 rounded-2xl border-4 border-green px-6 py-4 shadow-md flex flex-col gap-2 overflow-y-auto z-10">
+                <h2 className="text-2xl tracking-wider font-bold">
+                  All Orders
+                </h2>
+                <div className="w-full h-fit px-3 py-3 cursor-default bg-white border-green/50 border-2 rounded-2xl">
+                  <OrdersTable />
+                </div>
+              </div>
+              </div>
+             
             </div>
           </>
         ) : (
