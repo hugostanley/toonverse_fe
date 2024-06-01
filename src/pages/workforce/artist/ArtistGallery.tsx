88a8d97dad5a79f087b03db04279a21641a2e40b
jeanbejar <jeanevabejar@gmail.com>
2024-06-01 21:38:10 +0800

    chore: add query for artwork.

--- src/pages/workforce/artist/ArtistGallery.tsx
+++ src/pages/workforce/artist/ArtistGallery.tsx
@@ -1,16 +1,24 @@
 import Masonry from "react-masonry-css";
-import { categories } from "@assets";
+import { useQuery } from "@tanstack/react-query";
+import { ALL_ARTWORKS, apiClient } from "@utils";
 
 function ArtistGallery() {
-  const allImages = categories.flatMap((category) => category.sample);
-
-
+  const { data, isLoading, error } = useQuery<any>({
+    queryKey: ["allArtworks"],
+    queryFn: async () => {
+      const response = await apiClient.get(ALL_ARTWORKS);
+      console.log(response.data, "artwork");
+      return response.data;
+    },
+  });
 
   const breakpointColumnsObj = {
     default: 3,
     1100: 2,
     700: 1,
   };
+
+  const artworks = data?.artworks || [];
 
   return (
     <>
@@ -19,21 +27,35 @@ function ArtistGallery() {
           <h1 className="text-[2rem] font-bold text-yellow pl-6 pt-2 border-b-4 border-yellow drop-shadow-md">
             Artist Gallery
           </h1>
-          <Masonry
-            breakpointCols={breakpointColumnsObj}
-            className="my-masonry-grid p-4 overflow-auto h-screen overflow-x-hidden "
-            columnClassName="my-masonry-grid_column"
-          >
-            {allImages.map((bg, bgIndex) => (
-              <div key={bgIndex} className="my-masonry-grid_item min-w-full h-fit hover:scale-125">
-                <img
-                  src={bg}
-                  className="w-full h-auto rounded-xl shadow-2xl"
-                  alt={`background-${bgIndex}`}
-                />
-              </div>
-            ))}
-          </Masonry>
+          {isLoading && (
+            <div className="w-full h-[80vh] flex-center">
+              <div className="loader"></div>
+            </div>
+          )}
+          {error && <p>Error loading artworks. Please try again later.</p>}
+          {!isLoading && !error && artworks.length === 0 && (
+            <p>No available data</p>
+          )}
+          {artworks.length > 0 && (
+            <Masonry
+              breakpointCols={breakpointColumnsObj}
+              className="my-masonry-grid p-4 overflow-auto h-screen overflow-x-hidden"
+              columnClassName="my-masonry-grid_column"
+            >
+              {artworks.map((artwork: any, artworkIndex: number) => (
+                <div
+                  key={artworkIndex}
+                  className="my-masonry-grid_item min-w-full h-fit hover:scale-125"
+                >
+                  <img
+                    src={artwork.artwork_url}
+                    className="w-full h-auto rounded-xl shadow-2xl"
+                    alt={`background-${artworkIndex}`}
+                  />
+                </div>
+              ))}
+            </Masonry>
+          )}
         </div>
       </main>
     </>
